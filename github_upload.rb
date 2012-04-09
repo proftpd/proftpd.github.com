ba995d6e95273058a67ec15e88ddc8ad7b087217
Michael Renner <robe@master.proftpd.org>
2012-04-09 08:27:03 +0000

    Refactor script a bit

--- github_upload.rb
+++ github_upload.rb
@@ -5,18 +5,19 @@ require 'net/github-upload'
 require 'yaml'
 require 'erubis'
 
-repo = 'proftpd/proftpd-downloads'
-prefix = "https://github.com/downloads/proftpd/proftpd-downloads/"
 
 config = YAML.load_file("github_upload.conf")
-
 localdir = config["localdir"]
+repo = config["downloadrepo"]
 
 gh = Net::GitHub::Upload.new(
 	:login => config["login"],
 	:token => config["token"]
 )
 
+downloadbaseurl = "https://github.com/downloads/%s/" % [ repo ]
+
+puts "*** Building file lists from remote repository and local directory"
 remotelisting = gh.list_files(repo)
 localfiles = Dir.entries(localdir).delete_if { |f| f[0] == '.' }
 
@@ -28,12 +29,12 @@ versions.each do |version|
 	entry = Hash[
 		:version => version,
 		:mtime => File.mtime("%s/%s.tar.gz" % [localdir, version]),
-		:gzlink => "%s%s.tar.gz" % [prefix, version],
-		:gzsig => "%s%s.tar.gz.asc" % [prefix, version],
-		:gzmd5 => "%s%s.tar.gz.md5" % [prefix, version],
-		:bz2link => "%s%s.tar.bz2" % [prefix, version],
-		:bz2sig => "%s%s.tar.bz2.asc" % [prefix, version],
-		:bz2md5 => "%s%s.tar.bz2.md5" % [prefix, version]
+		:gzlink => "%s%s.tar.gz" % [downloadbaseurl, version],
+		:gzsig => "%s%s.tar.gz.asc" % [downloadbaseurl, version],
+		:gzmd5 => "%s%s.tar.gz.md5" % [downloadbaseurl, version],
+		:bz2link => "%s%s.tar.bz2" % [downloadbaseurl, version],
+		:bz2sig => "%s%s.tar.bz2.asc" % [downloadbaseurl, version],
+		:bz2md5 => "%s%s.tar.bz2.md5" % [downloadbaseurl, version]
 	]
 	file_listing.push entry
 end
@@ -46,25 +47,25 @@ remotelisting.each { |f| remotefiles[f[:name]] = f[:id] }
 to_delete = remotefiles.keys - localfiles
 to_upload = localfiles - remotefiles.keys
 
-if to_upload.count == 0 && to_delete.count == 0
-	puts "No new/deleted files, exiting"
-#	exit
-end
 
 to_delete.each do |file|
-	puts "Deleting from github: " + file
+	puts "*** Deleting from github: " + file
 	gh.delete(repo, remotefiles[file])
 end
 
 to_upload.each do |file|
-	puts "Uploading to github: " + file
+	puts "*** Uploading to github: " + file
 	gh.upload(:repos => repo, :file => [localdir, file].join('/'))
 end
+
+puts "*** Generating index.html"
 
 erb = Erubis::Eruby.new(File.read('index.html.erb'))
 File.open('index.html', 'w') { |f|
 	f.write(erb.result(:downloads => file_listing))
 }
+
+puts "*** Pushing index.html to github"
 
 cmd = "git commit index.html -m \"auto-update: %s\"" % [ Time.now() ]
 system(cmd)
